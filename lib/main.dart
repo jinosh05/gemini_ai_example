c2093e62de579c2c0feec3d5a89c0169d3e8aa95
Jinosh05 <jinoshprabhuraj5@gmail.com>
2024-02-18 13:15:21 +0530

    Base Created

--- lib/main.dart
+++ lib/main.dart
@@ -6,33 +6,88 @@ void main() async {
   runApp(const MainApp());
 }
 
-class MainApp extends StatefulWidget {
+class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
-  State<MainApp> createState() => _MainAppState();
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: ChatPage(),
+    );
+  }
 }
 
-class _MainAppState extends State<MainApp> {
+class ChatPage extends StatefulWidget {
+  const ChatPage({
+    super.key,
+  });
+
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  TextEditingController controller = TextEditingController();
+  List<String> listDatas = [];
+  bool isLoading = false;
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: ElevatedButton(
-            onPressed: () async {
-              final model = GenerativeModel(
-                model: 'gemini-pro',
-                apiKey: geminiApiKey,
-              );
-
-              const prompt = 'Tell me about Yourself';
-              final content = [Content.text(prompt)];
-              final response = await model.generateContent(content);
-
-              print(response.text);
-            },
-            child: const Text("Hello")),
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      appBar: AppBar(
+        title: const Text('Gemini AI Generator'),
       ),
+      bottomSheet: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            Expanded(
+              child: TextField(
+                controller: controller,
+                decoration: InputDecoration(
+                  contentPadding: const EdgeInsets.all(10),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              width: 5,
+            ),
+            InkWell(
+              onTap: () async {
+                if (controller.text.isNotEmpty) {
+                  listDatas.add(controller.text);
+                  setState(() {
+                    isLoading = true;
+                  });
+                  final model = GenerativeModel(
+                    model: 'gemini-pro',
+                    apiKey: geminiApiKey,
+                  );
+
+                  final prompt = controller.text;
+                  final content = [Content.text(prompt)];
+                  final response = await model.generateContent(content);
+                  listDatas.add(response.text ?? "");
+                  setState(() {
+                    isLoading = false;
+                  });
+                  controller.clear();
+                }
+              },
+              child: const Icon(
+                Icons.send,
+                size: 25,
+                color: Colors.blue,
+              ),
+            )
+          ],
+        ),
+      ),
+      body: ElevatedButton(onPressed: () async {}, child: const Text("Hello")),
     );
   }
 }
